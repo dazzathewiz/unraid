4809f7c632fd6436f748e76f287b0184fd7cad1e
dazzathewiz <39952432+dazzathewiz@users.noreply.github.com>
2020-02-26 06:25:36 +1000

    Create downloadkeyfile.sh

---
+++ downloadkeyfile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+file=path/to/file
+bucket=your-bucket
+resource="/${bucket}/${file}"
+contentType="application/x-compressed-tar"
+dateValue="`date +'%a, %d %b %Y %H:%M:%S %z'`" 
+stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=xxxxxxxxxxxxxxxxxxxx
+s3Secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+signature=`/bin/echo -en "$stringToSign" | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -H "Host: ${bucket}.s3.amazonaws.com" -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${s3Key}:${signature}" -o /root/keyfile https://${bucket}.s3.amazonaws.com/${file}
